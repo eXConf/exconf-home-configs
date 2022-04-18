49ad242a4d14a70c869a0221c5e1fb527c4a2db6
eXConf <sergey.sosedkin@gmail.com>
2022-04-18 12:07:30 +0300

    added some abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,18 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Abbreviations
+
+# Navigation
+abbr .. 'cd ..'
+
+# Git
+abbr gs 'git status'
+abbr gcm 'git commit -m'
+abbr gad 'git add .'
+
+# NordVPN
 abbr london 'nordvpn connect London'
 abbr disco 'nordvpn disconnect'
+
+# End of Abbreviations
